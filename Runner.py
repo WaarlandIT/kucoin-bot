fb43982fda8152bb9a0dc2145b3a7f6820cea1f8
Ron Pool <ron.pool@cyso.group>
2024-04-24 12:50:22 +0200

    modified:   Runner.py

--- Runner.py
+++ Runner.py
@@ -8,6 +8,6 @@ m_client = Market(url='https://api.kucoin.com')
 
 my_coin = 'BTC'
 
-my_data = m_client.get_ticker(my_ucoin + '-USDT')
+my_data = m_client.get_ticker(my_coin + '-USDT')
 
 print(my_data)
