390602f1579a559723235e1de6c96c639ec6010c
Ron Pool <ron.pool@cyso.group>
2024-04-24 12:49:14 +0200

    new file:   Runner.py

---
+++ Runner.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+import pandas as pd
+
+from kucoin.client import Market
+
+m_client = Market(url='https://api.kucoin.com')
+
+my_coin = 'BTC'
+
+my_data = m_client.get_ticker(my_ucoin + '-USDT')
+
+print(my_data)
