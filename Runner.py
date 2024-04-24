d75b27a28522b9090528a7176f780c53684cf723
Ron Pool <ron.pool@cyso.group>
2024-04-24 13:27:28 +0200

    modified:   Runner.py

--- Runner.py
+++ Runner.py
@@ -9,15 +9,18 @@ from kucoin.client import Market
 my_config = '/opt/kucoin-bot/runner.yml'
 m_client = Market(url='https://api.kucoin.com')
 
+with open(my_config, 'r') as file:
+    my_doc = yaml.safe_load(file)
+
 my_coin = 'BTC'
 
 my_data = m_client.get_ticker(my_coin + '-USDT')
 
 mydb = mysql.connector.connect(
-  host="localhost",
-  user="yourusername",
-  password="yourpassword",
-  database="mydatabase"
+  host = my_doc{'mysql'}['host'],
+  user = my_doc['mysql']['user'],
+  password = my_doc['mysql']['pass'],
+  database = my_doc['mysql']['database']
 )
 
 print(my_data)
