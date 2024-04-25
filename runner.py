ab3a002176c274a382bdc3cc47c71aa88d934f13
Ron Pool <ron.pool@cyso.group>
2024-04-25 11:03:17 +0200

    modified:   runner.py

--- runner.py
+++ runner.py
@@ -50,9 +50,9 @@ while True:
     
     my_start_time = int(my_current_time) - 300
     print(my_start_time)
-    my_select = 'SELECT price FROM coins where coin = ' + my_coin + ' AND time > ' + my_start_time + ';'
+    my_select = 'SELECT price FROM coins where coin = %s AND time > %i;'
 
-    mycursor.execute(my_select)
+    mycursor.execute(my_select, my_coin, my_start_time)
     my_result = mycursor.fetchall()
     for x in my_result:
       print(x)
